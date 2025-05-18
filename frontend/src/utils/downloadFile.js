c7abace955d8d6bf81442e1d765850881ff14653
Chris West <westremarketing@gmail.com>
2025-05-18 14:01:11 -0600

    feat: add transcript export options

---
+++ frontend/src/utils/downloadFile.js
@@ -0,0 +1,9 @@
+export function downloadFile(content, filename, type = "text/plain") {
+  const blob = new Blob([content], { type });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.download = filename;
+  link.href = url;
+  link.click();
+  URL.revokeObjectURL(url);
+}
