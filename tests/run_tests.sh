7ff81cfee06b8b56aa2537a594ba0e455becaba4
clwest <chris@donkeybetz.com>
2025-05-29 13:44:26 -0600

    clean up

--- tests/run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-cd "$(dirname "$0")/.."/backend
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-pytest "$@"
--- tests/test_thought_integrity.py
+++
@@ -1,9 +0,0 @@
-from assistants.utils.thought_integrity import analyze_thought_integrity
-
-
-def test_analyze_thought_integrity():
-    assert analyze_thought_integrity("") == "empty"
-    assert analyze_thought_integrity("   ") == "empty"
-    assert analyze_thought_integrity("```json") == "markdown_stub"
-    assert analyze_thought_integrity("ERROR: something") == "error_log"
-    assert analyze_thought_integrity("This is a valid thought.") == "valid"
