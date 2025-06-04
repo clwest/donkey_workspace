88ea3956fbf831e6ddee74e5c8c5da046b2e1ae9
Chris West <westremarketing@gmail.com>
2025-06-03 19:01:12 -0600

    refactor: share is_chunk_clean

--- backend/intel_core/utils/__init__.py
+++ backend/intel_core/utils/__init__.py
@@ -1,8 +1,13 @@
-from .document_cleanup import dedupe_document_chunks, update_document_status
+from .document_cleanup import (
+    dedupe_document_chunks,
+    update_document_status,
+    is_chunk_clean,
+)
 from .processing import save_document_to_db
 
 __all__ = [
     "dedupe_document_chunks",
     "update_document_status",
+    "is_chunk_clean",
     "save_document_to_db",
 ]
