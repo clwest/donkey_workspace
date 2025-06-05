7323e2f5a62527a2c8b791d9ef024eccc1a6a074
Chris West <westremarketing@gmail.com>
2025-06-04 18:06:31 -0600

    Fix reflect_on_self import

--- backend/assistants/views/__init__.py
+++ backend/assistants/views/__init__.py
@@ -10,7 +10,6 @@ from .assistants import (
     self_reflect,
     self_assess,
     reflect_on_assistant,
-    reflect_on_self,
     clarify_prompt,
     failure_log,
 )
@@ -168,7 +167,6 @@ __all__ = [
     "add_document_to_assistant",
     "self_reflect",
     "self_assess",
-    "reflect_on_self",
     "reflect_on_assistant",
     "submit_assistant_thought",
     "submit_chat_feedback",
