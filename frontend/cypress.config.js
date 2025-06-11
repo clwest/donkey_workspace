65db60aaeea2909bb477b42924de5307efc0184b
Chris West <westremarketing@gmail.com>
2025-06-10 19:16:50 -0600

    chore: set cypress supportFile

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/integration/**/*.spec.js',
-    baseUrl: 'http://localhost:5173'
+    baseUrl: 'http://localhost:5173',
+    supportFile: false
   }
 })
