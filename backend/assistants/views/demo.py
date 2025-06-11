038fa72c898d43fd35f26aaec0bb9bab469ebed4
clwest <chris@donkeybetz.com>
2025-06-10 18:33:52 -0600

    Updated imports to demo.py

--- backend/assistants/views/demo.py
+++ backend/assistants/views/demo.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, throttle_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.response import Response
 import uuid
