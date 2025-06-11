cc2fe81814ed9bdb22afcd209d3845c99043d411
Chris West <westremarketing@gmail.com>
2025-06-10 19:10:18 -0600

    fix cypress config for esm

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
   e2e: {
-    specPattern: "cypress/integration/**/*.spec.js",
-    baseUrl: "http://localhost:5173"
+    specPattern: 'cypress/integration/**/*.spec.js',
+    baseUrl: 'http://localhost:5173'
   }
-};
+})
