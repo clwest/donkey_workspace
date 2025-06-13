d55b1245e6f825f1a8a2754391cd8bdaf93eb524
clwest <chris@donkeybetz.com>
2025-06-12 20:28:17 -0600

    added imports to dev

--- backend/devtools/views.py
+++ backend/devtools/views.py
@@ -1,7 +1,8 @@
 from django.urls import get_resolver, URLPattern, URLResolver
 from django.views.decorators.cache import never_cache
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
+
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
